cd25ff234a04450c253a34a1a6829f68ed1faa70
petersid2022 <petersid2022@gmail.com>
2024-12-18 22:40:32 +0200

    Updated part2

--- src/impls/part2/stack.c
+++ src/impls/part2/stack.c
@@ -1,9 +1,8 @@
 #include <rt_misc.h>
 
-__value_in_regs struct __initial_stackheap __user_initial_stackheap(
-    unsigned R0, unsigned SP, unsigned R2, unsigned SL) {
+__value_in_regs struct __initial_stackheap __user_initial_stackheap(unsigned R0, unsigned SP, unsigned R2, unsigned SL) {
     struct __initial_stackheap config;
-    config.heap_base = 0x007110e0;
-    config.stack_base = 0x00a99510;
+    config.heap_base = 0x00391ff8;
+    config.stack_base = 0x0066fff0;
     return config;
 }
